ca956bacf17426e6b1e0cf335a4058e124715b1a
Weloz <daniel@baiao.dev>
2026-09-01 08:46:13 +0200

    feat(dev-env): add Docker development stack

---
+++ infrastructure/postgres/init-databases.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE lagonadeck_identity;
+CREATE DATABASE lagonadeck_catalog;
+CREATE DATABASE lagonadeck_inventory;
+CREATE DATABASE lagonadeck_sales;
+CREATE DATABASE lagonadeck_analytics;
+CREATE DATABASE lagonadeck_media;
